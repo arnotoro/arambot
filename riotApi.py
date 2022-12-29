74b60c9d029e66f9529de5c5ae7f5c36be2f1bf4
arnotoro <arno.toro@gmail.com>
2022-12-29 23:56:21 +0200

    riot api key changes

--- riotApi.py
+++ riotApi.py
@@ -1,15 +1,23 @@
 import requests
 import json
+from dotenv import load_dotenv
+import os 
+
+
+load_dotenv()
+
+RIOT_API = os.getenv('RIOT_API')
+
 
 def getSummonerID(summonerName):
-    api1 = f'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key=RGAPI-052be87f-343a-4b38-baa5-72ade8533fe9'
+    api1 = f'https://euw1.api.riotgames.com/lol/summoner/v4/summoners/by-name/{summonerName}?api_key={RIOT_API}'
     res = requests.get(api1)
     summonerID = json.loads(res.text)["id"]
     return summonerID
 
 
 def getMatch(summonerID):
-    api = f'https://euw1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/{summonerID}?api_key=RGAPI-052be87f-343a-4b38-baa5-72ade8533fe9'    
+    api = f'https://euw1.api.riotgames.com/lol/spectator/v4/active-games/by-summoner/{summonerID}?api_key={RIOT_API}'    
     
 
     res2 = requests.get(api)
